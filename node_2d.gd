5a8bff36b9de278bde9a880fc247a64f76bfd032
Gustavo Lopes <gustavolr035@gmail.com>
2026-04-06 20:56:04 -0300

    Add 3D shaders, assets and test scene; update 2D node

--- node_2d.gd
+++ node_2d.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
@@ -7,3 +7,6 @@ var acumulador : float = 1.0
 var guará
 var _asdasd
 var asdasd
+
+func _ready() -> void:
+	var allisson = "Alisson" if has_node("Allison") else null
